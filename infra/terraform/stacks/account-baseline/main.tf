0a1cdaede3ec540be6ed915c7e40664b558e3e82
Oren Lion <orenlion@Orens-MacBook-Pro.local>
2026-05-29 16:52:50 -0400

    Normalize account baseline service setting ID

--- infra/terraform/stacks/account-baseline/main.tf
+++ infra/terraform/stacks/account-baseline/main.tf
@@ -1,3 +1,12 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+locals {
+  ssm_default_host_management_setting_path = "/ssm/managed-instance/default-ec2-instance-management-role"
+  ssm_default_host_management_setting_arn  = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:servicesetting${local.ssm_default_host_management_setting_path}"
+}
+
 resource "aws_iam_role" "ssm_default_host_management" {
   name = "AWSSystemsManagerDefaultEC2InstanceManagementRole"
   assume_role_policy = jsonencode({
@@ -27,7 +36,7 @@ resource "aws_iam_role_policy_attachment" "ssm_default_host_management" {
 }
 
 resource "aws_ssm_service_setting" "default_ec2_instance_management_role" {
-  setting_id    = "/ssm/managed-instance/default-ec2-instance-management-role"
+  setting_id    = local.ssm_default_host_management_setting_arn
   setting_value = aws_iam_role.ssm_default_host_management.name
 
   depends_on = [aws_iam_role_policy_attachment.ssm_default_host_management]
--- infra/terraform/stacks/account-baseline/outputs.tf
+++ infra/terraform/stacks/account-baseline/outputs.tf
@@ -4,6 +4,6 @@ output "ssm_default_host_management_role_name" {
 }
 
 output "ssm_default_host_management_service_setting_id" {
-  value       = aws_ssm_service_setting.default_ec2_instance_management_role.setting_id
+  value       = local.ssm_default_host_management_setting_path
   description = "Account-wide SSM service setting ID managed by this stack."
 }
